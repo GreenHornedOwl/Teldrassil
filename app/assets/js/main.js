3d7e2d09bb20c748b6dc40d24147dd60ae440b3c
Daniel Voicu <daniel.voicu@brandfusion.ro>
2016-07-14 18:10:16 +0300

    menu
    mobile menu

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,29 @@
 $(function(){
 
-	console.log("Test");
+  
+  function menuTranslate(arg){
+     $('.mobile-wrapper').css("transform", 'translateX('+arg+'px)');
+  }
+  var mobileMenu = {
+    "opened": false
+  };
+  console.log(mobileMenu);
+  $('.mobile-wrapper').on("showMenu", menuTranslate("300"));
+  $('.mobile-wrapper').on("hideMenu", menuTranslate("0"));
+	$('.mobile-menu-trigger').on("click", function(){
+    if (mobileMenu.opened) {
+      $('.mobile-wrapper').trigger("hideMenu");
+      mobileMenu.opened = false;
+      console.log("true");
+    } else {
+      $('.mobile-wrapper').trigger("showMenu");
+      mobileMenu.opened = true;
+      console.log("false");
+    }
+    
+  });
+
+
+  
 
 });
